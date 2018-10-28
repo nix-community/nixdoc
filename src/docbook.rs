3b391f869701e212c7b2569ab06e273dbfe41275
Vincent Ambo <mail@tazj.in>
2018-10-28 16:58:52 +0100

    chore: License under GPLv3

--- src/docbook.rs
+++ src/docbook.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2018 Vincent Ambo <mail@tazj.in>
+//
+// nixdoc is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 //! This module implements DocBook XML output for a struct
 //! representing a single entry in th emanual.
 
