611b8b76d84c6b28e2411d9de1819f13b371b817
Lorenzo Manacorda <lorenzo@mailbox.org>
2023-03-22 22:09:04 +0100

    flake: add flake-compat

--- flake.nix
+++ flake.nix
@@ -1,9 +1,14 @@
 {
   description = "nixdoc";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-compat, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       {
